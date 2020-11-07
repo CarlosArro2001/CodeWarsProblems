3eff7aa8b990a5d6b22e332b253c04abf516df1c
CarlosArro2001 <51369125+CarlosArro2001@users.noreply.github.com>
2020-11-07 10:02:40 +0300

    Add files via upload

---
+++ squareNsum.py
@@ -0,0 +1,25 @@
+'''
+Question:
+Complete the square sum function so that it squares each 
+number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 
+because 1^2 + 2^2 + 2^2 = 9.
+'''
+
+# numbers to be pass
+
+num = [1, 2, 2]
+sum = 0
+
+
+def CompleteSquareSum():
+    x = 0
+    for i in num:
+        y = i ** 2
+        x += y
+        i += 1
+    print(x)
+
+
+CompleteSquareSum()
